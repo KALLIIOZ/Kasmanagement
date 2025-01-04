819de0659495f37654ee781b7783967abcdf0f38
KALLIIOZ <alancastillejos3@gmail.com>
2025-01-04 01:52:18 -0600

    added actor and contac_us urls

--- KasIndex/urls.py
+++ KasIndex/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
 
 urlpatterns=[
-    path('', views.index_view, name='index')
-]
+    path('', views.index_view, name='index'),
+    path('actor', views.actor_view, name='actor'),
+    path('contact_us', views.contact_view, name='contact_us')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
