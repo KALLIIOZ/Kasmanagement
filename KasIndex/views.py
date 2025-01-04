1390009ed96c7caba5f9a28890b67637e1ca42aa
KALLIIOZ <alancastillejos3@gmail.com>
2025-01-04 01:51:20 -0600

    added actor and contact_us views

--- KasIndex/views.py
+++ KasIndex/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, 'KasIndex/index.html')
+
+def actor_view(request):
+    return render(request, 'KasIndex/actor.html')
+
+def contact_view(request):
+    return render(request, 'KasIndex/contact_us.html')
