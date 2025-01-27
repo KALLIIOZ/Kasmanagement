4a4c217f0b3791b7e16f74214809e9e200b513e9
Alan <alancastillejos3@gmail.com>
2025-01-27 00:58:45 -0600

    Added view called to DB to obtain jsut speciala ctors and his/her names and pictures to show it on the index view

--- KasIndex/views.py
+++ KasIndex/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Actor
 
-# Create your views here.
 def index_view(request):
-    return render(request, 'KasIndex/index.html')
+    actores_especiales = Actor.objects.filter(especial=True)  # Obtén solo los actores especiales
+    return render(request, 'KasIndex/index.html', {'actores_especiales': actores_especiales})
 
 def actor_view(request):
     return render(request, 'KasIndex/actor.html')
